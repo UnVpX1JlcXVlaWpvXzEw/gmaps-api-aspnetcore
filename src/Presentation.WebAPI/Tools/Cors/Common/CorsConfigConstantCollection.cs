68f80f4145e6e9797f8cf3589bf0ac670ff6e573
RRequeijo <al79138@alunos.utad.pt>
2024-08-13 23:49:11 +0100

    APEX-330 / APEX-331 / APEX-332

---
+++ src/Presentation.WebAPI/Tools/Cors/Common/CorsConfigConstantCollection.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CorsConfigConstantCollection.cs" company="ApexAlgorithms">
+//     Copyright (c) ApexAlgorithms. All rights reserved.
+// </copyright>
+// <summary>
+// CorsConfigConstantCollection
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GMapsMagicianAPI.Presentation.WebAPI.Tools.Cors.Common
+{
+    /// <summary>
+    /// <see cref="CorsConfigConstantCollection"/>
+    /// </summary>
+    internal static class CorsConfigConstantCollection
+    {
+        /// <summary>
+        /// Gets the cors allowed origins.
+        /// </summary>
+        /// <value>The cors allowed origins.</value>
+        private static CorsConfigConstant CorsAllowedOrigins => new("Cors:AllowedOrigins");
+
+        /// <summary>
+        /// Gets the name of the cors origin collection.
+        /// </summary>
+        /// <value>The name of the cors origin collection.</value>
+        private static CorsConfigConstant CorsOriginCollectionName => new("Cors:OriginCollectionName");
+
+        /// <summary>
+        /// Gets the cors allowed origins.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        /// <returns></returns>
+        internal static string[] GetCorsAllowedOrigins(IConfiguration configuration)
+            => configuration.GetSection(CorsAllowedOrigins.Value).Get<string[]>();
+
+        /// <summary>
+        /// Gets the name of the cors origin collection.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        /// <returns></returns>
+        internal static string GetCorsOriginCollectionName(IConfiguration configuration)
+                    => configuration.GetSection(CorsOriginCollectionName.Value).Value;
+    }
+}
---
+++ src/Presentation.WebAPI/Tools/Cors/Configuration/CorsInjectionExtension.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CorsInjectionExtension.cs" company="ApexAlgorithms">
+//     Copyright (c) ApexAlgorithms. All rights reserved.
+// </copyright>
+// <summary>
+// CorsInjectionExtension
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GMapsMagicianAPI.Presentation.WebAPI.Tools.Cors.Configuration
+{
+    using GMapsMagicianAPI.Presentation.WebAPI.Tools.Cors.Common;
+    using GMapsMagicianAPI.Presentation.WebAPI.Tools.Cors.Configuration;
+
+    /// <summary>
+    /// <see cref="CorsInjectionExtension"/>
+    /// </summary>
+    public static class CorsInjectionExtension
+    {
+        /// <summary>
+        /// Adds the cors.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <param name="configuration">The configuration.</param>
+        public static void AddCors(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+                options.AddPolicy(CorsConfigConstantCollection.GetCorsOriginCollectionName(configuration), builder =>
+                {
+                    builder.WithOrigins(CorsConfigConstantCollection.GetCorsAllowedOrigins(configuration))
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+        }
+
+        /// <summary>
+        /// Uses the cors.
+        /// </summary>
+        /// <param name="app">The application.</param>
+        /// <param name="configuration">The configuration.</param>
+        public static void UseCors(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            app.UseCors(CorsConfigConstantCollection.GetCorsOriginCollectionName(configuration));
+
+            app.UseHttpsRedirection();
+        }
+    }
+}
