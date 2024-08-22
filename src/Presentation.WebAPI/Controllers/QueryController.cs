3a98463ebe2afb4b684771b24b9bc0ddf6527d80
RRequeijo <al79138@alunos.utad.pt>
2024-08-22 23:12:50 +0100

    APEX-339: Implement Delete Method

--- src/Presentation.WebAPI/Controllers/QueryController.cs
+++ src/Presentation.WebAPI/Controllers/QueryController.cs
@@ -11,7 +11,8 @@ namespace GMapsMagicianAPI.Presentation.WebAPI.Controllers
 {
     using AutoMapper;
     using GMapsMagicianAPI.Domain.AgregateModels.Query;
-    using GMapsMagicianAPI.Presentation.WebAPI.Commands.Query;
+    using GMapsMagicianAPI.Presentation.WebAPI.Commands.CreateQueryCommand;
+    using GMapsMagicianAPI.Presentation.WebAPI.Commands.DeleteQueryCommand;
     using GMapsMagicianAPI.Presentation.WebAPI.Dtos.Input.Query;
     using GMapsMagicianAPI.Presentation.WebAPI.Dtos.Output.Query;
     using GMapsMagicianAPI.Presentation.WebAPI.Queries.Query;
@@ -70,6 +71,27 @@ namespace GMapsMagicianAPI.Presentation.WebAPI.Controllers
             }, cancellationToken);
 
             return this.Created(string.Empty, this.mapper.Map<QueryDetailsDto>(query));
+        }
+
+        /// <summary>
+        /// Deletes the query asynchronous.
+        /// </summary>
+        /// <param name="filter">The filter.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        [HttpDelete("{Uuid}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(ErrorMessage), (int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> DeleteQueryAsync([FromRoute] DeleteQueryDto filter, CancellationToken cancellationToken)
+        {
+            await this.mediator.Publish(new DeleteQueryCommand
+            {
+                Uuid = filter.Uuid
+            }, cancellationToken);
+
+            return this.Ok();
         }
 
         /// <summary>
