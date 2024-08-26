ba8bca0dcca3c0c246438b5c3b77579b22499d0d
RRequeijo <al79138@alunos.utad.pt>
2024-08-26 19:54:32 +0100

    APEX-342: GetUnscrappedQuery

---
+++ src/Presentation.WebAPI/Dtos/Input/Query/GetUnscrappedQueryDto.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetUnscrappedQueryDto.cs" company="ApexAlgorithms">
+//     Copyright (c) ApexAlgorithms. All rights reserved.
+// </copyright>
+// <summary>
+// GetUnscrappedQueryDto
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GMapsMagicianAPI.Presentation.WebAPI.Dtos.Input.Query
+{
+    /// <summary>
+    /// <see cref="GetUnscrappedQueryDto"/>
+    /// </summary>
+    public class GetUnscrappedQueryDto
+    {
+        /// <summary>
+        /// Gets a value indicating whether this instance is instant.
+        /// </summary>
+        /// <value><c>true</c> if this instance is instant; otherwise, <c>false</c>.</value>
+        public bool IsInstant { get; init; }
+    }
+}
---
+++ src/Presentation.WebAPI/Queries/GetUnscrappedQueryQuery/GetUnscrappedQueryQuery.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GetUnscrappedQueryQuery.cs" company="ApexAlgorithms">
+//     Copyright (c) ApexAlgorithms. All rights reserved.
+// </copyright>
+// <summary>
+// GetUnscrappedQueryQuery
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GMapsMagicianAPI.Presentation.WebAPI.Queries.GetUnscrappedQueryQuery
+{
+    using GMapsMagicianAPI.Domain.AgregateModels.Query;
+    using MediatR;
+
+    /// <summary>
+    /// <see cref="GetUnscrappedQueryQuery"/>
+    /// </summary>
+    /// <seealso cref="IRequest{Query}"/>
+    public class GetUnscrappedQueryQuery : IRequest<Query>
+    {
+        /// <summary>
+        /// Gets a value indicating whether this instance is instant.
+        /// </summary>
+        /// <value><c>true</c> if this instance is instant; otherwise, <c>false</c>.</value>
+        public bool IsInstant { get; init; }
+    }
+}
